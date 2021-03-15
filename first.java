3b8ffe937ab3f5edb3d22e57f9866d734a93c4b6
DIVYA SRI TUMMALA <divya.20mis7094@gmail.com>
2021-03-15 15:55:05 +0530

    made changes to first.java program

--- first.java
+++ first.java
@@ -1,6 +1,8 @@
-// this is empty file
+// This is my first file Hello.java 
 import java.util.Scanner;
-class first{
+class Main{
 	public static void main(String args[]){
 		System.out.println("Hello java");
+	
 	}
+}
