caf1b1577e6d891e75f9748fafbe90e42db47810
DIVYA SRI TUMMALA <divya.20mis7094@gmail.com>
2021-03-23 11:25:02 +0530

    changes made to june rel

--- first.java
+++ first.java
@@ -2,7 +2,9 @@
 import java.util.Scanner;
 class Main{
 	public static void main(String arg[]){
+		int a,b,c;
 		System.out.println("Hello java");
+		System.out.println("This is my june-release code");
 	
 	}
 }
